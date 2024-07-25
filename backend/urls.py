06834337d89681ac63c38a926ddafc753a72600f
Nahid <nahidthedeveloper@gmail.com>
2024-07-26 05:57:19 +0600

    add dj rest auth

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from django.conf import settings
+from django.conf.urls.static import static
 
 from authentication.viewset import AuthenticationViewSet
 
@@ -11,4 +13,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include(router.urls)),
+    path('rest-auth/', include('rest_framework.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
